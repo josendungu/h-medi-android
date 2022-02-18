6d6575a4b58336b8d75c5a62ed9547efc55df726
Joe <josendungu.jm@gmail.com>
2022-02-18 15:24:25 +0300

    Colors, theme and typography

--- app/src/main/java/com/sylvia/h_medi/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/sylvia/h_medi/presentation/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Background = Color(0xFFF8FBFF)
+val Blue = Color(0xFF1565C0)
+val GreyText = Color(0xFF949494)
+val GreyLine = Color(0xFFEDEDED)
--- app/src/main/java/com/sylvia/h_medi/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/sylvia/h_medi/presentation/ui/theme/Type.kt
@@ -5,24 +5,59 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.sylvia.h_medi.R
 
-// Set of Material typography styles to start with
+
+private val Montserrat = FontFamily(
+    Font(R.font.montserrat_black, FontWeight.W700),
+    Font(R.font.montserrat_bold, FontWeight.W600),
+    Font(R.font.montserrat_regular, FontWeight.W400),
+    Font(R.font.montserrat_light, FontWeight.W300)
+)
+
+
 val Typography = Typography(
+
+    h1 = TextStyle(
+        fontWeight = FontWeight.ExtraBold,
+        fontFamily = Montserrat,
+        fontSize = 30.sp,
+    ),
+    h2 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontFamily = Montserrat,
+        fontSize = 24.sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = Montserrat,
+        fontSize = 20.sp
+    ),
+    h4 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = Montserrat,
+        fontSize = 16.sp
+    ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontFamily = Montserrat,
+        fontSize = 15.sp
+    ),
+    h6 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontFamily = Montserrat,
+        fontSize = 12.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    body2 = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = Montserrat,
+        fontSize = 14.sp
     )
-    */
+
 )
